0de56979e17eca587afc6f502dd3304827b06732
atabegruslan <ruslan_aliyev_@hotmail.com>
2020-04-18 15:29:19 +0700

    more edits

--- Readme.md
+++ Readme.md
@@ -1,12 +1,19 @@
 
 # Blockchain 
 
+`npm run start`
+
 - https://www.youtube.com/watch?v=SSo_EIwHSd4 
 - https://www.youtube.com/playlist?list=PLzvRQMJ9HDiTqZmbtFisdXFxul5k0F-Q4
 
 # Bitcoin 
 
+`npm run start`
+
 - https://www.youtube.com/playlist?list=PLU52pNodXIGdM6XDgHVG7DsPytlsrR_6b 
 - https://www.youtube.com/user/m1xolyd1an/playlists
 
 - https://www.blockchain.com/api/charts_api
+- https://www.blockchain.com/api/blockchain_api
+- https://www.npmjs.com/package/bitcore-lib
+- https://www.blockchain.com/api/exchange_rates_api
--- bitcoin/app.js
+++ bitcoin/app.js
@@ -5,21 +5,42 @@ var app = express();
 
 var request = require("request");
 
+var bodyparser = require("body-parser");
+var bitcore = require("bitcore-lib");
+
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+app.use(bodyparser.json());
+
+app.set("view engine", "ejs"); // By default, looks for views/
+
 const PORT = 8080;
 
-// http.createServer(function(req, res) {
+function brainWallet(userInput, callback)
+{
+	var input = Buffer.from(userInput);
+	var hash = bitcore.crypto.Hash.sha256(input);
+	var bn = bitcore.crypto.BN.fromBuffer(hash);
+	var pk = new bitcore.PrivateKey(bn).toWIF();
+	var address = new bitcore.PrivateKey(bn).toAddress();
+	callback(pk, address);
+};
 
-	// request({
-	// 	url: "https://api.blockchain.info/stats?format=json",
-	// 	json: true
-	// }, function(error, response, body) {
-	// 	console.log(response);
-	// });
+/*
+http.createServer(function(req, res) {
 
-// 	console.log("Hi I'm a new BitCoin user " + req.url);
-// 	res.end("End");
+	request({
+		url: "https://api.blockchain.info/stats?format=json",
+		json: true
+	}, function(error, response, body) {
+		console.log(response);
+	});
 
-// }).listen(PORT);
+	console.log("Hi I'm a new BitCoin user " + req.url);
+	res.end("End");
+
+}).listen(PORT);
 
 request({
 	url: "https://api.blockchain.info/stats?format=json",
@@ -35,6 +56,43 @@ app.get("/", function(req, res) { // http://localhost:8080/
 app.get("/block", function(req, res) { // http://localhost:8080/block
 	res.send({btcBlocks});
 });
+*/
+
+function getMarketPrice(callback) {
+	request({
+		url: "https://blockchain.info/ticker?format=json",
+	}, function(err, res, body) {
+		price = callback(JSON.parse(body).USD.last);
+	});
+}
+
+app.get("/", function(req, res) {
+	res.render("index");
+});	
+app.get("/wallet", function(req, res) { // http://localhost:8080/wallet
+	//res.sendFile(__dirname + '/index.html');
+
+	res.render("wallet");
+});
+app.get("/converter", function(req, res) {
+	getMarketPrice(function(marketPrice) {
+		res.render("converter", {
+			marketPrice: marketPrice
+		});
+	});
+});
+app.post("/wallet", function(req, res) {
+	var brainsrc = req.body.brainsrc;
+	brainWallet(brainsrc, function(priv, addr) {
+		//res.send({priv, addr});
+
+		res.render("wallet", {
+			url1: 'http://blockchain.info/rawaddr/' + addr.toString(),
+			url2: 'https://www.blockchain.com/explorer'
+		});
+	});
+});
+
 app.listen(PORT, function() {
-	console.log("Server is running");
+	console.log("Server is running at localhost:" + PORT);
 });
